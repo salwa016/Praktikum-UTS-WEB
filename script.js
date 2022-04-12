13371fd03f05b3cbb7e40f9d753180c1ea0fd4aa
salwa016 <alifiwafani@gmail.com>
2022-04-12 18:09:19 +0800

    random color

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let box = document.getElementById('box');
+const box = document.getElementById('box');
 document.onmousemove = (event) => {
     box.style.left = event.clientX - (box.clientWidth/2) + 'px'; 
     box.style.top  = event.clientY - (box.clientHeight/2)+ 'px';
